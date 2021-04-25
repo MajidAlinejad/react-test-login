bf04d88904045f925445f4a74b457a448330e6bf
majidAlinejad <alinejad.mj@gmail.com>
2021-04-25 16:36:36 +0430

    sidebar complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Layout, Breadcrumb, ConfigProvider } from "antd";
+import faIR from "antd/lib/locale/fa_IR";
+import { Component } from "react";
+import Header from "../src/Components/Header/Header";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Search from "./Components/Content/Search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Content } = Layout;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ConfigProvider locale={faIR} direction="rtl">
+          <Layout>
+            <Header/>
+            <Layout className="container-sider">
+            <Sidebar/>
+            <Layout className="container-section">
+              <Search/>
+            </Layout>
+            </Layout>
+           
+           
+          </Layout>
+        </ConfigProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Layout, Menu, Badge, Avatar } from "antd";
+import {
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined,
+  } from "@ant-design/icons";
+  
+const { SubMenu } = Menu;
+const {  Sider } = Layout;
+export default class Sidebar extends Component {
+    render() {
+        return (
+            <div className="sidebar">
+                <Sider width={200} className="site-layout-background">
+                <Menu
+                  mode="inline"
+                  defaultSelectedKeys={["1"]}
+                  defaultOpenKeys={["sub1"]}
+                  style={{ height: "100%", borderRight: 0 }}
+                >
+              
+                  <SubMenu key="sub1" icon={<UserOutlined />} title="منو 1">
+                    <Menu.Item key="1">زیر منو 1</Menu.Item>
+                    <Menu.Item key="2">زیر منو 2</Menu.Item>
+                    <Menu.Item key="3">زیر منو 3</Menu.Item>
+                    <Menu.Item key="4">زیر منو 4</Menu.Item>
+                  </SubMenu>
+                  <SubMenu
+                    key="sub2"
+                    icon={<LaptopOutlined />}
+                    title="منو 2"
+                  >
+                    <Menu.Item key="5">زیر منو 5</Menu.Item>
+                    <Menu.Item key="6">زیر منو 6</Menu.Item>
+                    <Menu.Item key="7">زیر منو 7</Menu.Item>
+                    <Menu.Item key="8">زیر منو 8</Menu.Item>
+                  </SubMenu>
+                  <SubMenu
+                    key="sub3"
+                    icon={<NotificationOutlined />}
+                    title="منو 3"
+                  >
+                    <Menu.Item key="9">زیر منو 9</Menu.Item>
+                    <Menu.Item key="10">زیر منو 10</Menu.Item>
+                    <Menu.Item key="11">زیر منو 11</Menu.Item>
+                    <Menu.Item key="12">زیر منو 12</Menu.Item>
+                  </SubMenu>
+                </Menu>
+              </Sider>
+            </div>
+        )
+    }
+}
